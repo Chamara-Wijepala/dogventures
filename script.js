9e1ce88cb23b289f8d601c71ab452c177fbb6664
Chamara-Wijepala <mr.worf777@gmail.com>
2024-10-18 20:56:32 +0530

    Add tours section

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const openNavBtn = document.getElementById('open-nav-btn');
 const closeNavBtn = document.getElementById('close-nav-btn');
 const mobileNav = document.getElementById('mobile-nav');
+const imageSlider = document.getElementById('image-slider');
+
+let isDragging = false;
+let startX, startScrollLeft;
 
 function toggleMobileNav() {
 	mobileNav.classList.toggle('translate-x-full');
@@ -9,5 +13,31 @@ function toggleMobileNav() {
 	mobileNav.classList.remove('duration-0');
 }
 
+function dragSlider(e) {
+	if (!isDragging) return;
+
+	imageSlider.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+function dragStart(e) {
+	isDragging = true;
+	startX = e.pageX;
+	startScrollLeft = imageSlider.scrollLeft;
+
+	imageSlider.classList.add('cursor-grabbing');
+	imageSlider.classList.add('select-none');
+}
+
+function dragStop() {
+	isDragging = false;
+
+	imageSlider.classList.remove('cursor-grabbing');
+	imageSlider.classList.remove('select-none');
+}
+
 openNavBtn.addEventListener('click', toggleMobileNav);
 closeNavBtn.addEventListener('click', toggleMobileNav);
+
+imageSlider.addEventListener('mousemove', dragSlider);
+imageSlider.addEventListener('mousedown', dragStart);
+imageSlider.addEventListener('mouseup', dragStop);
